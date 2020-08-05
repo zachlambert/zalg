02c3fbad42f46bbd3c66853282b2cb0143064fa4
Zach Lambert <zachalambert@gmail.com>
2020-08-05 18:15:01 +0100

    Updated dynamic array with DataType object

--- include/structures/dynamic_array.h
+++ include/structures/dynamic_array.h
@@ -1,21 +1,12 @@
 #ifndef ZALG_STRUCTURES_DYNAMIC_ARRAY_H
 #define ZALG_STRUCTURES_DYNAMIC_ARRAY_H
 
-
-// ** Dynamically sized array (unordered) **
-// Doubles in size when more memory is required
-// Start pointer points to first item
-// End pointer points to one-past the last item
-// Add items to the end of the array
-// When deleting an item, replaces it with the last item, so doesn't
-// need a function to free the daya
-// * If the data contains pointers to memory that has to be freed
-//   the user has to free this memory before removing the data
-
 #include <stdlib.h>
+#include "data_type.h"
+
 
 typedef struct {
-    size_t data_size;
+    DataType data_type;
     size_t capacity;
     void *start;
     void *end;
@@ -23,7 +14,7 @@ typedef struct {
 
 DynamicArray dynamic_array_create(size_t data_size);
 void dynamic_array_add(DynamicArray *array, void *data);
-void dynamic_array_remove(DynamicArray *array, void *data);
+void dynamic_array_remove(DynamicArray *array, size_t index);
 void dynamic_array_destroy(DynamicArray *array);
 
 #endif
--- src/structures/dynamic_array.c
+++ src/structures/dynamic_array.c
@@ -15,7 +15,7 @@ DynamicArray dynamic_array_create(size_t data_size)
 void dynamic_array_add(DynamicArray *array, void *data)
 {
     if (!data) return;
-    if (array->end - array->start == array->capacity * array->data_size) {
+    if (array->end - array->start == array->capacity * array->data_type.size) {
         if (array->capacity == 0) {
             array->capacity = 1;
         } else {
@@ -25,19 +25,27 @@ void dynamic_array_add(DynamicArray *array, void *data)
         array->end = new_start + (array->end - array->start);
         array->start = new_start;
     }
-    memcpy(array->end, data, array->data_size);
-    array->end += array->data_size;
+    memcpy(array->end, data, array->data_type.size);
+    array->end += array->data_type.size;
 }
 
-void dynamic_array_remove(DynamicArray *array, void *data)
+void dynamic_array_remove(DynamicArray *array, size_t index)
 {
-    if (!data) return;
-    array->end -= array->data_size;
-    memcpy(data, array->end, array->data_size);
+    if (index >= (array->end - array->start)/array->data_type.size) return;
+    array->end -= array->data_type.size;
+    array->data_type.copy(
+        array->end,
+        array->start + index * array->data_type.size
+    );
+    array->data_type.destroy(array->end);
 }
 
 void dynamic_array_destroy(DynamicArray *array)
 {
+    void *it;
+    while (it != array->end) {
+        array->data_type.destroy(it);
+    }
     free(array->start);
 }
 
